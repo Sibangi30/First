703ca58775e36f9156a99dbf54fa5524669ccc94
Sibangi30 <Sibangishree@gmail.com>
2026-05-21 20:03:57 +0530

    operations

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,1 +1,13 @@
 console.log("Hello world")
+var sum = 10 + 20 ;
+var dif = 34 - 10 ;
+var mul = 18 * 20 ;
+var quotient = 24/2 ;
+console.log(sum)
+console.log(dif)
+console.log(mul)
+console.log(quotient)
+var myvar ;
+myvar = 45 ;
+myvar++ ;
+console.log(myvar)
