f477aa94870165fe888323d62aed66c35a4391cf
Sibangi30 <Sibangishree@gmail.com>
2026-05-29 22:45:16 +0530

    String positions

---
+++ Javascript/varcode.js
@@ -0,0 +1,8 @@
+var firstname = "theveryfirstname"
+var lastletter = firstname[firstname.length-1];
+var secondlastletter = firstname[firstname.length-2];
+var thirdlastletter = firstname[firstname.length-3];
+console.log(firstname)
+console.log(lastletter)
+console.log(secondlastletter)
+console.log(thirdlastletter)
