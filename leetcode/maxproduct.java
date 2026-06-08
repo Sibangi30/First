5e9ce8950de74caed81eef5c191ebeaeb0a5ef47
Sibangi30 <Sibangishree@gmail.com>
2026-06-08 23:19:16 +0530

    solution to Subarray Sum Equals K

---
+++ leetcode/maxproduct.java
@@ -0,0 +1,16 @@
+//Subarray Sum Equals K
+class maxproduct {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            int sum = 0;
+            for (int end = start; end < nums.length; end++) {
+                sum += nums[end];  
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
