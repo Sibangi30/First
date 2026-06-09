a04107c5f819398bc3acb46f2ab2dd627edac23c
Sibangi30 <Sibangishree@gmail.com>
2026-06-09 23:26:58 +0530

    Modifying array

---
+++ Javascript/arpush.js
@@ -0,0 +1,12 @@
+var myarr = [["a","b","c"],["dog" ,"cat","cow"]];
+myarr.push(["d","e"]) ;
+console.log(myarr)
+
+var removedfrommyarr = myarr.pop() ;
+console.log(myarr);
+
+var removedfrommyarr = myarr.shift()
+myarr.shift();
+myarr.unshift("Happy")
+console.log(myarr)
+
