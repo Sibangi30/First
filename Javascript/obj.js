7c581d62b9f93fdfa261513b0557b6febd91155a
Sibangi30 <Sibangishree@gmail.com>
2026-06-12 23:29:48 +0530

    use of this keyword in OOPs

--- Javascript/obj.js
+++ Javascript/obj.js
@@ -7,3 +7,14 @@ console.log(person.name); // Output: Amit
 person.city = "Bhubaneswar";
 
 console.log(person.age)
+
+const person1 = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+console.log(person1.fullname)
